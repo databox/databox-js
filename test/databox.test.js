55570f4e1c22d64dcf983aac46363f55e2b00eaa
Oto Brglez <otobrglez@gmail.com>
2015-06-23 12:53:05 +0200

    Adding testunit

--- test/databox.test.js
+++ test/databox.test.js
@@ -1,6 +1,21 @@
 var path = require('path');
+// var test = require('testunit');
 var assert = require('assert');
 
 var Databox = require(path.join('../lib/', 'databox.js'));
 
-assert(10 === 12, 'This is the same');
+module.exports = {
+    setUp: function (cb) {
+        cb();
+    },
+
+    testSetup: function (test) {
+        var push_token = 123;
+        var d = new Databox({
+            'push_token': push_token
+        });
+
+        test.ok(d.push_token == push_token, "Token must be set");
+        test.done();
+    }
+}
