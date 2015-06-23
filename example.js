55b2a43e3287d5b889755915fa79c4c901c1766c
Oto Brglez <otobrglez@gmail.com>
2015-06-23 12:35:24 +0200

    Adding structure.

---
+++ example.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var Databox = require('./lib/databox');
+
+var client = new Databox({
+    'push_token': 'adxg1kq5a4g04k0wk0s4wkssow8osw84',
+});
+
---
+++ test/databox.test.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var assert = require('assert');
+
+var Databox = require(path.join('../lib/', 'databox.js'));
+
+assert(10 === 12, 'This is the same');
